bf5d3126e8043d099703a3f7c70736cfd87f60b9
MDThaher <thahergithub@gmail.com>
2023-12-04 19:05:54 +0530

    new changes

--- Hello.java
+++ Hello.java
@@ -1,4 +1,9 @@
 public class Hello {
+    
+    public static void sum(){
+        System.out.println(52 + 1);
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome to java");
     }
